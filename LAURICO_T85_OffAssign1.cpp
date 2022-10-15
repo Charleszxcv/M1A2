c5edd715ae0d5a84644ceff2dc9bdc0b86fa5815
Charleszxcv <113902006+Charleszxcv@users.noreply.github.com>
2022-10-15 12:57:17 +0800

    Update LAURICO_T85_OffAssign1.cpp

--- LAURICO_T85_OffAssign1.cpp
+++ LAURICO_T85_OffAssign1.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
-#include <gl\glut.h>
+#include <glut.h>
 
 using namespace std;
 
 void display();
-void reshape(int, int);
+void reshape(int,int);
 
 void init()
 {
-
-	glClearColor(1.0, 0.0, 0.0, 1.0);
+	//set background to black
+	glClearColor(1.0,0.2,0.0,0.0);
 }
 
-int main(int argc, char** argv)
+int main(int argc,char**argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-
-	glutInitWindowPosition(200, 200);
-	glutInitWindowSize(500, 500);
-	glutCreateWindow("LAURICO_M1A2_T85");
-
-	glutDisplayFunc(display);
-	glutReshapeFunc(reshape);
-
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE); //display modes
+	
+	glutInitWindowPosition(200,100); //window postion
+	glutInitWindowSize(500,500); //window size
+	glutCreateWindow ("LAURICO_M1A2"); //create the initialized window with name
+	
+	glutDisplayFunc(display); //initialize display function
+	glutReshapeFunc(reshape); //initialize reshape function
+	
 	init();
-	glutMainLoop();
+	glutMainLoop();//loop at the functions
 
 }
 
 void display()
 {
+	
 
-	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glClear(GL_COLOR_BUFFER_BIT);//clear color
+	
+	glLoadIdentity();//clear transforms
 
-	glLoadIdentity();
-
-	  glBegin(GL_LINE_LOOP);
+	
+	 glBegin(GL_LINE_LOOP);
 	glColor3f(0.0f, 0.0f, 0.0f);
 	glVertex2f(0.0f, -16.0f);
 	glVertex2f(11.0f, -12.0f);
@@ -80,8 +83,8 @@ void display()
 	glEnd();
 	glLineWidth(3);
 
-	  glBegin(GL_POLYGON); \
-    glColor3f(1.0f, 0.6f, 0.0f);
+	  glBegin(GL_POLYGON);
+        glColor3f(1.0f, 0.6f, 0.0f);
 	glVertex2f(0.0f, -16.0f);
 	glVertex2f(11.0f, -12.0f);
 	glVertex2f(8.0f, -9.0f);
@@ -157,14 +160,17 @@ void display()
 	glVertex2f(-11.0f, -12.0f);
 	glEnd();
 
-	glutSwapBuffers();
+   
+
+	
+	glutSwapBuffers(); //Send the scene to the screen
 }
 
 void reshape(int w, int h)
 {
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glViewport(0,0,(GLsizei)w, (GLsizei)h);
 	glMatrixMode(GL_PROJECTION);
 	//size of the world
-	gluOrtho2D(-16, 16, -16, 16);
+	gluOrtho2D(-10,10,-10,10);
 	glMatrixMode(GL_MODELVIEW);
 }
